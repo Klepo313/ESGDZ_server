b9982a677dafe14a4911e2c24bd1c528f8af75b0
„klepo313” <antonio.klepo1@gmail.com>
2024-05-26 14:46:32 +0200

    update queries.js - dodavanje ubijanja sesije na kraju queriea.

--- queries.js
+++ queries.js
@@ -2,72 +2,86 @@ const Pool = require('pg').Pool;
 
 // Postavljanje sigurnih parametara za PostgreSQL Pool
 const pool = new Pool({
-  host: 'localhost',
-  user: 'postgres',
-  database: 'ESGDZ',
-  password: '2c9MeDrU!?',
+  host: 'cornelius.db.elephantsql.com',
+  user: 'rwjrfavq',
+  database: 'rwjrfavq',
+  password: '32RW27yZLBMJ4walViHjKsH06lnUKRC7',
   port: 5432,
+  // max: 20, // Povećajte max vrijednost prema potrebi
 });
+// const pool = new Pool({
+//   host: 'localhost',
+//   user: 'postgres',
+//   database: 'ESGDZ',
+//   password: '2c9MeDrU!?',
+//   port: 5432,
+// });
 
-pool.connect(); // Povezivanje s bazom podataka
+// pool.connect(); // Povezivanje s bazom podataka - nije potrebno jer pool automatski upravlja konekcijama
 
 async function loginUser(username, password) {
-
   const usernameRegex = /^[a-zA-Z]+$/; // Regularni izraz koji provjerava da li string sadrži samo slova
 
   if (!usernameRegex.test(username)) {
     throw new Error('Korisničko ime smije sadržavati samo slova.');
-  }
-  else {
+  } else {
+    const client = await pool.connect();
     try {
       const query = 'SELECT eko_id, eko_par_id_za, eko_korime, eko_zaporka FROM esg_korisnici WHERE eko_korime = $1 AND eko_zaporka = $2 AND CURRENT_DATE BETWEEN eko_datod AND eko_datdo;';
-      const result = await pool.query(query, [username, password]);
+      const result = await client.query(query, [username, password]);
 
       if (result.rows.length === 0) {
         throw new Error('Korisnik nije pronađen.');
       }
-      
-      const user = result.rows[0]
-      
-      return { 
-        eko_id: user.eko_id, 
+
+      const user = result.rows[0];
+
+      return {
+        eko_id: user.eko_id,
         eko_par_id_za: user.eko_par_id_za,
         eko_korime: user.eko_korime,
       };
     } catch (error) {
       throw error;
+    } finally {
+      client.release();
     }
   }
 }
 
 const getVrsteUpitnika = async (req, res) => {
+  const client = await pool.connect();
   try {
-    //const query = 'SELECT evu_id, evu_sif, evu_naziv, evu_kreirano, epu_datdo FROM esg_veza_par_evu, esg_vrste_upitnika WHERE epu_evu_id = 5364362454 AND epu_par_id = $1';
-    const query = 'select * from edz_struktura  where ess_ess_id is null  order by rbr';
-    //const result = await pool.query(query, [firmId]);
-    const result = await pool.query(query);
-    
+    const query = 'select * from edz_struktura where ess_ess_id is null order by rbr';
+    const result = await client.query(query);
+
     res.json(result.rows);
   } catch (error) {
     throw error;
+  } finally {
+    client.release();
   }
-}
+};
 
 const getUpitniciForUser = async (req, res) => {
+  const client = await pool.connect();
   try {
     const query = 'select ezu_id, evu_sif, evu_naziv, ezu_datum, ezu_ezp_id, ezu_par_id, ezu_kreirano, ezu_mijenjao, ezu_ess_id from esg_zag_upitnik, edz_struktura where ezu_kreirao = $1 and ezu_par_id = $2 and ess_id = ezu_ess_id order by ezu_kreirano desc';
-    const result = await pool.query(query, [req.params.userName, parseInt(req.params.firmId)]);
+    const result = await client.query(query, [req.params.userName, parseInt(req.params.firmId)]);
 
     res.json(result.rows);
   } catch (error) {
     throw error;
+  } finally {
+    client.release();
   }
-}
+};
 
 const getUpitnik = async (req, res) => {
+  const client = await pool.connect();
   try {
     const query = 'SELECT * FROM edz_struktura WHERE evu_sif = $1';
-    const result = await pool.query(query, [req.params.evu_sif]);
+    const result = await client.query(query, [req.params.evu_sif]);
 
     const rows = result.rows;
 
@@ -112,22 +126,28 @@ const getUpitnik = async (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(500).send('Error retrieving group data');
+  } finally {
+    client.release();
   }
 };
 
 const getGroupsData = async (req, res) => {
+  const client = await pool.connect();
   try {
     const query = 'select ess_id, ess_ess_id, ess_vrsta, ess_naziv, razina, ukpitanja from edz_struktura where ess_ess_id = $1';
-    const result = await pool.query(query, [req.params.ess_id]);
+    const result = await client.query(query, [req.params.ess_id]);
 
     res.json(result.rows);
   } catch (error) {
     console.error(error);
     res.status(500).send('Error retrieving group data');
+  } finally {
+    client.release();
   }
 };
 
 const getQuestionsForGroup = async (req, res) => {
+  const client = await pool.connect();
   try {
     const questionsQuery = `
     select eou_id, ept_id, ept_ess_id, ept_rbr, ept_pitanje, ept_vrstaodg, ept_jedmjer, ept_format, ept_opis, ept_uvjetovan, p.*, o.* 
@@ -139,7 +159,7 @@ const getQuestionsForGroup = async (req, res) => {
     order by ept_rbr
     `;
 
-    const questionsResult = await pool.query(questionsQuery, [parseInt(req.params.p_ezu_id), parseInt(req.params.p_ess_id)]);
+    const questionsResult = await client.query(questionsQuery, [parseInt(req.params.p_ezu_id), parseInt(req.params.p_ess_id)]);
 
     const questions = questionsResult.rows;
 
@@ -151,7 +171,7 @@ const getQuestionsForGroup = async (req, res) => {
           WHERE eso_ept_id = $1
           ORDER BY eso_id
         `;
-        const possibleAnswersResult = await pool.query(possibleAnswersQuery, [question.ept_id]);
+        const possibleAnswersResult = await client.query(possibleAnswersQuery, [question.ept_id]);
         question.possibleAnswers = possibleAnswersResult.rows;
       }
     }
@@ -160,67 +180,66 @@ const getQuestionsForGroup = async (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(500).send('Error retrieving group data');
+  } finally {
+    client.release();
   }
 };
 
 const getAnswersForUpitnik = async (req, res) => {
+  const client = await pool.connect();
   try {
     const query = 'SELECT * FROM esg_odg_upitnik WHERE eou_ezu_id = $1 ORDER BY eou_id'; // and eou_sadrzaj is not null 
-    const result = await pool.query(query, [parseInt(req.params.p_ezu_id)]);
+    const result = await client.query(query, [parseInt(req.params.p_ezu_id)]);
 
     res.json(result.rows);
   } catch (error) {
-      console.error(error);
-      res.status(500).send('Error retrieving group data');
-    }
-}
-
-const saveAnswer = (p_eou_id, p_vrijednost, p_kor_id, callback) => {
-  try {
-    const query = 'SELECT Fn_upisi_odgovor($1, $2, $3) AS rezultat';
-    pool.query(query, [p_eou_id, p_vrijednost, p_kor_id], (err, result) => {
-      if (err) {
-        console.error('Error executing query:', err);
-        return callback(err);
-      }
-      // console.log('Query result:', result);
-      if (result && result.rows && result.rows.length > 0) {
-        callback(null, result.rows[0].rezultat);
-      } else {
-        console.error('No result returned from query');
-        callback(new Error('No result returned from query'));
-      }
-    });
-  } catch (error) {
-    console.error('Unexpected error:', error);
-    throw error;
+    console.error(error);
+    res.status(500).send('Error retrieving group data');
+  } finally {
+    client.release();
   }
 }
 
-const createNewUpitnik = (p_kor_id, p_evu_sif, callback) => {
+const saveAnswer = async (p_eou_id, p_vrijednost, p_kor_id, callback) => {
+  const client = await pool.connect();
+  try {
+    const query = 'SELECT Fn_upisi_odgovor($1, $2, $3) AS rezultat';
+    const result = await client.query(query, [p_eou_id, p_vrijednost, p_kor_id]);
+    if (result && result.rows && result.rows.length > 0) {
+      callback(null, result.rows[0].rezultat);
+    } else {
+      console.error('No result returned from query');
+      callback(new Error('No result returned from query'));
+    }
+  } catch (error) {
+    console.error('Error executing query:', error);
+    return callback(error);
+  } finally {
+    client.release();
+  }
+}
+
+const createNewUpitnik = async (p_kor_id, p_evu_sif, callback) => {
+  const client = await pool.connect();
   try {
     const query = 'SELECT dodaj_upitnik($1, CURRENT_DATE, $2) AS dodano';
-    pool.query(query, [p_kor_id, p_evu_sif], (err, result) => {
-      if (err) {
-        console.error('Error executing query:', err);
-        return callback(err);
-      }
-      // console.log('Query result:', result);
-      if (result && result.rows && result.rows.length > 0) {
-        callback(null, result.rows[0].dodano);
-      } else {
-        console.error('No result returned from query');
-        callback(new Error('No result returned from query'));
-      }
-    });
+    const result = await client.query(query, [p_kor_id, p_evu_sif]);
+    if (result && result.rows && result.rows.length > 0) {
+      callback(null, result.rows[0].dodano);
+    } else {
+      console.error('No result returned from query');
+      callback(new Error('No result returned from query'));
+    }
   } catch (error) {
-    console.error('Unexpected error:', error);
-    throw error;
+    console.error('Error executing query:', error);
+    return callback(error);
+  } finally {
+    client.release();
   }
 }
 
 module.exports = {
-  pool, 
+  pool,
   loginUser,
   getVrsteUpitnika,
   getUpitniciForUser,
